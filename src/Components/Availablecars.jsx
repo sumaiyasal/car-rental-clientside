562709619fff291f54e42368593b37b9a1264fa0
sumaiya salim <sumaiyasalim062@gmail.com>
2024-12-27 20:58:22 +0600

    added sorting in  available section functionality

--- src/Components/Availablecars.jsx
+++ src/Components/Availablecars.jsx
@@ -1,17 +1,31 @@
-import { useState } from "react";
-import { useLoaderData } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { Link, useLoaderData } from "react-router-dom";
 
 const Availablecars = () => {
-  
+  const [sortOption, setSortOption] = useState('dateAddedDesc');
   const[availcars,setAvailcars]=useState([]);
-  fetch(`${import.meta.env.VITE_API_URL}/cars`)
-  .then(res=>res.json())
-  .then(data=>setAvailcars(data))
- 
+  useEffect(() => {
+    
+      fetch(`${import.meta.env.VITE_API_URL}/availcars?sortOption=${sortOption}`)
+      .then(res=>res.json())
+      .then(data=>setAvailcars(data))
+    
+
+  }, [sortOption]);
+const handleSortChange = (e) => {
+    setSortOption(e.target.value);  
+  };
     return (
      <div className="home">
         <div className="container mx-auto">
         <h1 className='text-center text-4xl font-extrabold p-16 '>Latest Cars</h1>
+        <div className="sorting-options text-black pb-4">
+        <label className="text-white">Sort By: </label>
+        <select onChange={handleSortChange} value={sortOption}>
+          <option value="dateAddedDesc">Date (Newest First)</option>
+          <option value="dateAddedAsc">Date (Oldest First)</option>
+        </select>
+      </div>
           <div className='grid lg:grid-cols-3 grid-cols-1 gap-8 pl-4'> 
               {  availcars.map(lcar=>
                   <div className=''>
@@ -28,7 +42,7 @@ const Availablecars = () => {
       <p>Availability : {lcar.availability}</p>
       <p>Booking_count : {lcar.booking_count}</p>
       <p>Posted on :{lcar.date_posted}</p>
-
+       <Link to='/cardetails'><button className="btn bg-orange-500">Book Now</button></Link>
     </div>
   </div> 
                   </div>
